cac5ba896a532b6133d19d470d2acea9062fe37e
Akritisohane123 <akritisohane123@gmail.com>
2025-05-06 02:03:31 +0530

    Add files via upload

---
+++ create_tables.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Users (
+    user_id INT PRIMARY KEY,
+    name VARCHAR(100),
+    age INT,
+    weight FLOAT,
+    height FLOAT,
+    goal_calories INT
+);
+-- Add other tables as I gave earlier
